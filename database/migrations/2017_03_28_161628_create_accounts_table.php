6b253e8ef12c872e003d7635869c635678c5cb7a
crystal <jeremy85828@gmail.com>
2017-03-28 18:28:33 +0200

    WIP - added accounts table migration

---
+++ database/migrations/2017_03_28_161628_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('password', 60)->nullable();
+            $table->string('salt')->nullable();
+            $table->string('name');
+            $table->string('last_name');
+            $table->integer('payment_due');
+            $table->integer('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('accounts');
+    }
+}
