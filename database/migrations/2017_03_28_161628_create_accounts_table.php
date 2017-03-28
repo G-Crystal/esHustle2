c66db85e7b796c8a287b7ea5e0cb98726b952d51
crystal <jeremy85828@gmail.com>
2017-03-28 18:56:55 +0200

    WIP - updated accounts table migration

--- database/migrations/2017_03_28_161628_create_accounts_table.php
+++ database/migrations/2017_03_28_161628_create_accounts_table.php
@@ -17,10 +17,10 @@ class CreateAccountsTable extends Migration
             $table->string('email', 100)->unique();
             $table->string('password', 100);
             $table->string('salt', 45);
-            $table->string('name', 100);
-            $table->string('last_name', 100);
-            $table->integer('payment_due');
-            $table->integer('active');
+            $table->string('name', 100)->nullable();
+            $table->string('last_name', 100)->nullable();
+            $table->integer('payment_due')->nullable();
+            $table->integer('active')->nullable();
             $table->timestamps();
         });
     }
