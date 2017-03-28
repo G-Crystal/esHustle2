ca02cbced8e177718cefa908b7ff1d7029266154
crystal <jeremy85828@gmail.com>
2017-03-28 18:28:59 +0200

    WIP - added marketplace table migration

---
+++ database/migrations/2017_03_28_161855_create_marketplaces_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMarketplacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marketplaces', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('marketplace');
+            $table->string('region');
+            $table->string('endpoint');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('marketplaces');
+    }
+}
